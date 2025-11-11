d3a45927fc6d5d5ddcf2ceb826b9602ca99d9447
zerow.kim <0wkim@naver.com>
2025-11-11 15:32:47 +0900

    feat: 초성 제시어 추출

--- src/controller/InitialQuizController.js
+++ src/controller/InitialQuizController.js
@@ -1,3 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+import InitialModel from "../model/InitialModel.js";
+import CheckAnswerModel from "../model/CheckAnswerModel.js";
+
 export default class InitialQuizController {
-    
+    async run() {
+        const initialWord = InitialModel.getInitialWord();
+        Console.print(initialWord);
+    }
 }
--- src/model/InitialModel.js
+++ src/model/InitialModel.js
@@ -1,6 +1,16 @@
 export default class InitialModel {
-    getInitial() {
+    static getInitialWord() {
         const INITIAL = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
+        
+        let initialWord = "";
 
+        for (let i = 0; i < 2; i++) {
+            const randomIndex = Math.floor(Math.random() * INITIAL.length);
+            initialWord += INITIAL[randomIndex];
+        }
+
+        // ㅃㅊ, ㄲㅆ, ㄱㅉ, ㄱㅋ, ㄲㄴ, ㄲㄸ, ㄲㄹ, ㄲㅁ, ㄲㅃ, ㄲㅉ, ㄲㅋ, ㄲㅍ, ㄲㅎ, ㄴㄸ, ㄴㅃ, ㄴㅆ, ㄴㅉ, ㄷㅃ, ㄷㅆ, ㄷㅉ, ㄷㅋ, ㄷㅌ는 단어가 별로 없음 
+
+        return initialWord;
     }
 }
